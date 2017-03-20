cc36412ab8d511cbda267184d139da68dddf474d
Luke Rhodes <rhodesy22@gmail.com>
2017-03-20 11:18:39 +1100

    Updated to modern JS and added ability to check status to update HomeKit UI

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-homebridge-applescript
+homebridge-applescript-status
 ======================
 
-Supports triggering AppleScript commands on the HomeBridge platform.
+Supports triggering AppleScript commands on the HomeBridge platform using either files
+or text commands set in the config.
 
 ## Installation
 
 1. Install homebridge using: `npm install -g homebridge`
-2. Install this plugin using: `npm install -g homebridge-applescript`
-3. Update your configuration file. See `sample-config.json` in this repository for a sample.
+2. Install this plugin using: `npm install -g homebridge-applescript-status`
+3. Update your configuration file. See `config-sample.json` in this repository for a sample.
 
 ## Configuration
 
@@ -16,12 +17,28 @@ Configuration sample:
 ```
 "accessories": [
 	{
-		"accessory": "Applescript",
+		"accessory": "AppleScript",
 		"name": "Security Camera",
+    "type": "command",
 		"on": "tell application ''Evocam'' to open ''Security.evocamsettings''",
 		"off": "quit application ''Evocam''"
+	},
+	{
+		"accessory": "AppleScript",
+		"name": "Music",
+		"on": "/Users/rhodesy22/Documents/AppleScript/iTunesPlay.scpt",
+		"off": "/Users/rhodesy22/Documents/AppleScript/iTunesPause.scpt",
+		"status": "/Users/rhodesy22/Documents/AppleScript/iTunesStatus.scpt"
 	}
 ]
 ```
 
-Note that two successive single-quotes (`''`) will be automatically converted to double-quotes.
+## Notes
+
+Two successive single-quotes (`''`) will be automatically converted to double-quotes.
+
+Make sure the accessory name is "AppleScript" not "Applescript"
+
+## Credits
+
+Notable thanks to @dansays (https://github.com/dansays/homebridge-applescript) and @bendodson (https://github.com/bendodson/homebridge-applescript) who's code has been re-worked to make the core of this plugin.
--- index.js
+++ index.js
@@ -3,51 +3,80 @@ var Characteristic;
 
 var applescript = require('applescript');
 
-module.exports = function(homebridge) {
+module.exports = (homebridge) => {
 	Service = homebridge.hap.Service;
 	Characteristic = homebridge.hap.Characteristic;
-	homebridge.registerAccessory('homebridge-applescript', 'Applescript', ApplescriptAccessory);
+	homebridge.registerAccessory('homebridge-applescript-status', 'AppleScript', AppleScriptAccessory);
 }
 
-function ApplescriptAccessory(log, config) {
+const ApplescriptAccessory = (log, config) => {
 	this.log = log;
 	this.service = 'Switch';
-	this.name = config['name'];
-	this.onCommand = config['on'];
-	this.offCommand = config['off'];
+	this.config = config
 }
 
-ApplescriptAccessory.prototype.setState = function(powerOn, callback) {
-	var accessory = this;
-	var state = powerOn ? 'on' : 'off';
-	var prop = state + 'Command';
-	var command = accessory[prop].replace(/''/g, '"');
+class AppleScriptAccessory {
 
-	applescript.execString(command, done);
+	setState (powerOn, callback) {
+		const state = powerOn ? 'on' : 'off';
+		let command = this.config[state];
 
-	function done(err, rtn) {
-		if (err) {
-			accessory.log('Error: ' + err);
-			callback(err || new Error('Error setting ' + accessory.name + ' to ' + state));
+		if (this.config.type === 'file') {
+			applescript.execFile(command, done);
 		} else {
-			accessory.log('Set ' + accessory.name + ' to ' + state);
-			callback(null);
+			command = command.replace(/''/g, '"');
+
+			applescript.execString(command, done);
+		}
+
+		const done = (err) => {
+			if (err) {
+				accessory.log('Error: ' + err);
+				callback(err || new Error('Error setting ' + this.config.name + ' to ' + state));
+			} else {
+				accessory.log('Set ' + this.config.name + ' to ' + state);
+				callback(null);
+			}
 		}
 	}
-}
 
-ApplescriptAccessory.prototype.getServices = function() {
-	var informationService = new Service.AccessoryInformation();
-	var switchService = new Service.Switch(this.name);
+	retrieveSwitchState () {
+		let command = this.config.status;
+		if (!command) return
 
-	informationService
-		.setCharacteristic(Characteristic.Manufacturer, 'Applescript Manufacturer')
-		.setCharacteristic(Characteristic.Model, 'Applescript Model')
-		.setCharacteristic(Characteristic.SerialNumber, 'Applescript Serial Number');
+		if (this.config.type === 'file') {
+			applescript.execFile(command, done);
+		} else {
+			command = command.replace(/''/g, '"');
 
-	switchService
-		.getCharacteristic(Characteristic.On)
-		.on('set', this.setState.bind(this));
+			applescript.execString(command, done);
+		}
 
-	return [switchService];
+		const done = (err, response) => {
+			setTimeout(this.retrieveSwitchState, 1000);
+
+			if (err) return;
+
+			this.switchService.setCharacteristic(Characteristic.On, !!response);
+		}
+	}
+
+	getServices () {
+		const informationService = new Service.AccessoryInformation();
+		const switchService = new Service.Switch(this.config.name);
+
+		informationService
+			.setCharacteristic(Characteristic.Manufacturer, 'Applescript Manufacturer')
+			.setCharacteristic(Characteristic.Model, 'Applescript Model')
+			.setCharacteristic(Characteristic.SerialNumber, 'Applescript Serial Number');
+
+		switchService
+			.getCharacteristic(Characteristic.On)
+			.on('set', this.setState.bind(this));
+
+		this.switchService = switchService;
+		this.retrieveSwitchState();
+
+		return [switchService];
+	}
 }
